40594511458b314cd72b8fc2f920b59885997299
shumandi <shimandi@icloud.com>
2016-12-30 15:26:39 +0800

    30

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var async = require('async');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/routes')
 
 var app = express();
 
@@ -22,8 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
